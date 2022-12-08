ebbd20929524dcca4e826aa104da9b00e5360ddb
okyan123 <3181584445@qq.com>
2022-12-08 17:48:49 +0800

    格式化文档

--- src/testReport4.cpp
+++ src/testReport4.cpp
@@ -16,34 +16,28 @@ return 1;}
 class Point
 {
 private:
-    double x, y;
+	double x, y;
 
 public:
-    Point() : x(0), y(0) {}
-    Point(double x) : x(x), y(0) {}
-    Point(double x, double y) : x(x), y(y) {}
-    void Print1()
-    {
-        cout << "x=" << x << ", y=" << y << endl;
-    }
-    friend void Print2(Point &p);
+	Point() : x(0), y(0) {}
+	Point(double x) : x(x), y(0) {}
+	Point(double x, double y) : x(x), y(y) {}
+	void Print1() { cout << "x=" << x << ", y=" << y << endl; }
+	friend void Print2(Point &p);
 };
 
-void Print2(Point &p)
-{
-    cout << "x=" << p.x << ", y=" << p.y << endl;
-}
+void Print2(Point &p) { cout << "x=" << p.x << ", y=" << p.y << endl; }
 
 int main()
 {
-    Point P1;
-    Point P2(1.3);
-    Point P3(1.3, 5.6);
-    P1.Print1();
-    P2.Print1();
-    P3.Print1();
-    Print2(P1);
-    Print2(P2);
-    Print2(P3);
-    return 0;
+	Point P1;
+	Point P2(1.3);
+	Point P3(1.3, 5.6);
+	P1.Print1();
+	P2.Print1();
+	P3.Print1();
+	Print2(P1);
+	Print2(P2);
+	Print2(P3);
+	return 0;
 }
